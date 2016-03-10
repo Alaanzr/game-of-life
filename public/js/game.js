a5d122bc07e0232ddbd8c4be5d33f0fe7a1a4f6b
Alaanzr <Alaanzr@gmail.com>
2016-03-10 21:37:10 +0000

    Fixed bug with printCells function

--- public/js/game.js
+++ public/js/game.js
@@ -1,11 +1,3 @@
-function Cell() {
-  var SPAWNER = 0.8;
-  this.alive = Math.random() > SPAWNER;
-  this.neighbours = 0;
-}
-
-
-
 function Game(numRows, numCols) {
   this.gridSize = numRows * numCols;
   this.grid = this.generateGrid(numRows, numCols);
@@ -41,7 +33,7 @@ Game.prototype.printCells = function() {
   for (var i = 0; i < this.grid.numRows; i++) {
     var rowString = '';
     for (var j = 0; j < this.grid.numCols; j++) {
-      var cell = this.grid.cells[i * j];
+      var cell = this.grid.cells[(i * this.grid.numRows) + j];
       rowString += cell.alive ? 'X|' : ' |';
     }
     console.log(rowString);
