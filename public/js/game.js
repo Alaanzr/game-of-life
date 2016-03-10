9c4874ada906e1e75a8f9a4b5a67d9faf91eb1f2
Alaanzr <Alaanzr@gmail.com>
2016-03-10 23:41:23 +0000

    Working game

--- public/js/game.js
+++ public/js/game.js
@@ -32,6 +32,7 @@ Game.prototype.generateGrid = function(numRows, numCols) {
 };
 
 Game.prototype.printCells = function() {
+  console.log('\033[2J]');
   for (var i = 0; i < this.grid.numRows; i++) {
     var rowString = '';
     for (var j = 0; j < this.grid.numCols; j++) {
@@ -39,5 +40,65 @@ Game.prototype.printCells = function() {
       rowString += cell.alive ? 'X|' : ' |';
     }
     console.log(rowString);
+  }
+};
+
+Game.prototype.isUnderpopulated = function(rowIndex, columnIndex) {
+  var cell = this.grid.cells[rowIndex][columnIndex];
+  return cell.neighbours < 2;
+};
+
+Game.prototype.isOverpopulated = function(rowIndex, columnIndex) {
+  var cell = this.grid.cells[rowIndex][columnIndex];
+  return cell.neighbours > 3;
+};
+
+Game.prototype.isResurrectable = function(rowIndex, columnIndex) {
+  var cell = this.grid.cells[rowIndex][columnIndex];
+  return !cell.alive && cell.neighbours === 3;
+};
+
+Game.prototype.isInBounds = function(rowIndex, columnIndex) {
+  var rowIsInBounds = rowIndex >=0 && rowIndex < this.grid.numRows;
+  var columnIsInBounds = columnIndex >= 0 && columnIndex < this.grid.numCols;
+  return rowIsInBounds && columnIsInBounds;
+};
+
+Game.prototype.updateNeighbouringCell = function(rowIndex, columnIndex) {
+  var cell = this.grid.cells[rowIndex][columnIndex];
+  cell.neighbours = 0;
+  for (var i = 0; i < this.directions.length; i++) {
+    var direction = this.directions[i];
+    var rowDifference = rowIndex + direction[0];
+    var columnDifference = columnIndex + direction[1];
+    if (this.isInBounds(rowDifference, columnDifference)) {
+      var neighbour = this.grid.cells[rowDifference][columnDifference];
+      if (neighbour.alive) cell.neighbours++;
+    }
+  }
+};
+
+Game.prototype.updateNeighbours = function() {
+  for (var i = 0; i < this.grid.numRows; i++) {
+    for (var j = 0; j < this.grid.numCols; j++) {
+      this.updateNeighbouringCell(i, j);
+    }
+  }
+};
+
+Game.prototype.updateCellState = function(rowIndex, columnIndex) {
+  var cell = this.grid.cells[rowIndex][columnIndex];
+  if (this.isUnderpopulated(rowIndex, columnIndex) || this.isOverpopulated(rowIndex, columnIndex)) {
+    cell.alive = false;
+  } else if (this.isResurrectable(rowIndex, columnIndex)) {
+    cell.alive = true;
+  }
+};
+
+Game.prototype.updateCells = function() {
+  for (var i = 0; i < this.grid.numRows; i++) {
+    for (var j = 0; j < this.grid.numCols; j++) {
+      this.updateCellState(i, j);
+    }
   }
 };
